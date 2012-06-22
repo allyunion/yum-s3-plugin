59997c0b34d499997a3cf92af02d125ff3ce31f5
allyunion <allyunion@gmail.com>
2012-06-21 18:00:56 -0700

    Updating README, removing boto requirements

--- s3.py
+++ s3.py
@@ -26,10 +26,16 @@ examples on how to deploy those.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+#   This fork of the plugin only uses UrlGrabber and does not depend on python-boto
+
 import logging
 import os
 import sys
 import urllib
+import urllib2
+import time
+import hmac
+import base64
 
 from yum.plugins import TYPE_CORE
 from yum.yumRepo import YumRepository
@@ -39,7 +45,7 @@ from yum import logginglevels
 import yum.Errors
 
 # This plugin's revision number
-__revision__ = "1.1.0"
+__revision__ = "1.1.2"
 
 ##### Set up Yum Plugin #####
 
@@ -75,174 +81,90 @@ def init_hook(conduit):
 			repos.delete(repo.id)
 			repos.add(new_repo)
 
-##### Define Grabber Functions #####
+class UrllibGrabber:
+	def __init__(self, awsAccessKey, awsSecretKey, baseurl ):
+		self.logger = logging.getLogger("yum.verbose.main")
+		try:
+			baseurl = baseurl[0]
+		except:
+			pass
+		self.baseurl = baseurl
+		self.awsAccessKey = awsAccessKey
+		self.awsSecretKey = awsSecretKey
 
-def createUrllibGrabber():
+	def s3sign(self, cls, request, secret_key, key_id, date=None):
+		self.logger.log(logginglevels.DEBUG_4, "s3: Signing S3 URL...")
+       		date=time.strftime("%a, %d %b %Y %H:%M:%S +0000", date or time.gmtime() )
+       		host = request.get_host()
+       		bucket = host.split('.')[0]
+       		request.add_header( 'Date', date)
+       		resource = "/%s%s" % ( bucket, request.get_selector() )
+       		sigstring = """%(method)s\n\n\n%(date)s\n%(canon_amzn_resource)s""" % {
+		                               'method':request.get_method(),
+		                               #'content_md5':'',
+		                               #'content_type':'', # only for PUT
+		                               'date':request.headers.get('Date'),
+		                               #'canon_amzn_headers':'',
+		                               'canon_amzn_resource':resource }
+		try:
+			import hashlib
+			digest = hmac.new(secret_key, sigstring, hashlib.sha1 ).digest()
+		except:
+			import sha
+       			digest = hmac.new(secret_key, sigstring, sha ).digest()
+       		digest = base64.b64encode(digest)
+       		request.add_header('Authorization', "AWS %s:%s" % ( key_id,  digest ))
 
-	import urllib2
-	import time
-	import hmac
-	import base64
+	def _request(self,url):
+		from urllib import quote
+		self.logger.log(logginglevels.DEBUG_4, "s3: Requesting URL: %s%s" % (self.baseurl, quote(url)))
+		req = urllib2.Request("%s%s" % (self.baseurl, quote(url)))
+		self.s3sign(self, req, self.awsSecretKey, self.awsAccessKey )
+		return req
 
-	class UrllibGrabber:
-		@classmethod
-
-		def s3sign(cls,request, secret_key, key_id, date=None):
-        		date=time.strftime("%a, %d %b %Y %H:%M:%S +0000", date or time.gmtime() )
-        		host = request.get_host()
-        		bucket = host.split('.')[0]
-        		request.add_header( 'Date', date)
-        		resource = "/%s%s" % ( bucket, request.get_selector() )
-        		sigstring = """%(method)s\n\n\n%(date)s\n%(canon_amzn_resource)s""" % {
-			                               'method':request.get_method(),
-			                               #'content_md5':'',
-			                               #'content_type':'', # only for PUT
-			                               'date':request.headers.get('Date'),
-			                               #'canon_amzn_headers':'',
-			                               'canon_amzn_resource':resource }
-			try:
-				import hashlib
-				digest = hmac.new(secret_key, sigstring, hashlib.sha1 ).digest()
-			except:
-				import sha
-        			digest = hmac.new(secret_key, sigstring, sha ).digest()
-        		digest = base64.b64encode(digest)
-        		request.add_header('Authorization', "AWS %s:%s" % ( key_id,  digest ))
-
-		def __init__(self, awsAccessKey, awsSecretKey, baseurl ):
-			self.logger = logging.getLogger("yum.verbose.main")
-			try:
-				baseurl = baseurl[0]
-			except:
-				pass
-			self.baseurl = baseurl
-			self.awsAccessKey = awsAccessKey
-			self.awsSecretKey = awsSecretKey
-
-		def _request(self,url):
-			from urllib import quote
-			self.logger.log(logginglevels.DEBUG_4, "s3: Requesting URL: %s%s" % (self.baseurl, quote(url)))
-			req = urllib2.Request("%s%s" % (self.baseurl, quote(url)))
-			UrllibGrabber.s3sign(req, self.awsSecretKey, self.awsAccessKey )
-			return req
-
-		def urlgrab(self, url, filename=None, **kwargs):
-			"""urlgrab(url) copy the file to the local filesystem"""
-			self.logger.log(logginglevels.DEBUG_4, "s3: UrlLibGrabber urlgrab url=%s filename=%s" % ( url, filename ))
-			req = self._request(url)
-			if not filename:
-				filename = req.get_selector()
-				if filename[0] == '/':
-					filename = filename[1:]
-			out = open(filename, 'w+')
+	def urlgrab(self, url, filename=None, **kwargs):
+		"""urlgrab(url) copy the file to the local filesystem"""
+		self.logger.log(logginglevels.DEBUG_4, "s3: UrlLibGrabber urlgrab url=%s filename=%s" % ( url, filename ))
+		req = self._request(url)
+		if not filename:
+			filename = req.get_selector()
+			if filename[0] == '/':
+				filename = filename[1:]
+		out = open(filename, 'w+')
+		try:
 			resp = urllib2.urlopen(req)
 			buff = resp.read(8192)
 			while buff:
 				out.write(buff)
 				buff = resp.read(8192)
-			return filename
-			# zzz - does this return a value or something?
+		except urllib2.HTTPError, e:
+			self.logger.critical("s3: While grabbing url=%s filename=%s, urllib2 returned: %s" % ( url, filename, e ))
+			raise yum.Errors.RepoError('s3: urllib2: %s' % e)
+		except socket.error, e:
+			self.logger.critical("s3: While grabbing url=%s filename=%s, received a socket error: %s" % ( url, filename, e ))
+			raise
+		except socket.gaierror, e:
+			self.logger.critical("s3: While grabbing url=%s filename=%s, received a socket error: %s" % ( url, filename, e ))
+			raise
+		except socket.timeout, e:
+			self.logger.critical("s3: While grabbing url=%s filename=%s, received a socket error: %s" % ( url, filename, e ))
+			raise
+		except KeyboardInterrupt:
+			raise
+		except:
+			self.logger.critical("s3: While grabbing url=%s filename=%s, received an unknown error!")
+			raise
+			
+		return filename
+		# zzz - does this return a value or something?
 
-		def urlopen(self, url, **kwargs):
-			"""urlopen(url) open the remote file and return a file object"""
-			return urllib2.urlopen( self._request(url) )
+	def urlopen(self, url, **kwargs):
+		"""urlopen(url) open the remote file and return a file object"""
+		return urllib2.urlopen( self._request(url) )
 
-		def urlread(self, url, limit=None, **kwargs):
-			"""urlread(url) return the contents of the file as a string"""
-			return urllib2.urlopen( self._request(url) ).read()
-
-	return UrllibGrabber
-
-def createBotoGrabber():
-	import boto
-	import re
-
-	from urlparse import urlparse
-	from urlgrabber.grabber import URLGrabber
-
-	class BotoGrabber(URLGrabber):
-		def __init__(self, awsAccessKey, awsSecretKey, baseurl):
-			self.logger = logging.getLogger("yum.verbose.main")
-			self.logger.log(logginglevels.DEBUG_4, "s3: creating empty URLGrabber instance")
-			URLGrabber.__init__(self)
-			self.logger.log(logginglevels.DEBUG_4, "s3: BotoGrabber init BASE_URL=%s" % baseurl)
-			if not baseurl:
-				raise Exception("s3: BotoGrabberInit got blank baseurl")
-			try:
-				baseurl = baseurl[0]
-			except:
-				pass
-			self.s3 = boto.connect_s3(awsAccessKey, awsSecretKey)
-			self.baseurl = urlparse(baseurl)
-			if hasattr(self.baseurl, 'netloc'):
-				self.bucket_name = self.baseurl.netloc
-				self.key_prefix = self.baseurl.path[1:]
-			else:
-				self.bucket_name = self.baseurl[1]
-				self.key_prefix = self.baseurl[2]
-			if self.key_prefix.startswith("/"):
-				self.key_prefix = self.key_prefix[1:]
-			m = re.match('(.*)\.s3.*\.amazonaws\.com', self.bucket_name)
-			if (m):
-				self.bucket_name = m.group(1)
-			if sys.stdout.isatty():
-				print "%s - %s" % (self.bucket_name, self.key_prefix)
-
-		def _key_name(self,url):
-			self.logger.log(logginglevels.DEBUG_4, "s3: BotoGrabber _key_name url=%s, key_prefix=%s" % ( url, self.key_prefix ))
-			if not url.startswith("http://"):
-				return "%s%s" % ( self.key_prefix, url )
-			if sys.stdout.isatty():
-				print "Notice: extracting path from url (%s) instead of using prefix (%s)" % (url,self.key_prefix)
-			result = urlparse(url)[2]
-			if sys.stdout.isatty():
-				print "Notice: extracted path is: %s" % result
-			return result
-
-		def _key(self, key_name):
-			bucket = self.s3.get_bucket(self.bucket_name)
-			self.logger.log(logginglevels.DEBUG_4, "s3: BotoGrabber _key for bucket_name=%s, key_name=%s" % ( self.bucket_name, key_name ))
-			return bucket.get_key(key_name)
-
-		def urlgrab(self, url, filename=None, **kwargs):
-			"""urlgrab(url) copy the file to the local filesystem"""
-			self.logger.log(logginglevels.DEBUG_4, "s3: BotoGrabber urlgrab url=%s filename=%s" % ( url, filename ))
-			key_name = self._key_name(url)
-			self.logger.log(logginglevels.DEBUG_4, "s3: BotoGrabber urlgrab url=%s key_name=%s filename=%s" % ( url, key_name, filename ))
-			key = self._key(key_name)
-			if not key:
-				raise Exception("s3: Can not get key for key=%s" % key_name )
-
-			if not filename:
-				filename = key.key
-
-			key.get_contents_to_filename(filename)
-			return filename
-			# zzz - does this return a value or something?
-
-		def urlopen(self, url, **kwargs):
-			"""urlopen(url) open the remote file and return a file object"""
-			self.logger.log(logginglevels.DEBUG_4, "s3: BotoGrabber urlopen url=%s" % url)
-			return self._key(url)
-
-		def urlread(self, url, limit=None, **kwargs):
-			"""urlread(url) return the contents of the file as a string"""
-			self.logger.log(logginglevels.DEBUG_4, "s3: BotoGrabber urlread url=%s" % url)
-			return self._key(url).read()
-
-	return BotoGrabber
-
-def createGrabber():
-	logger = logging.getLogger("yum.verbose.main")
-	try:
-		rv = createBotoGrabber()
-		logger.log(logginglevels.DEBUG_4, "s3: Created BotoGrabber")
-		return rv
-	except:
-		logger.log(logginglevels.DEBUG_4, "s3: Creating UrllibGrabber")
-		return createUrllibGrabber()
-
-AmazonS3Grabber = createGrabber()
+	def urlread(self, url, limit=None, **kwargs):
+		"""urlread(url) return the contents of the file as a string"""
+		return urllib2.urlopen( self._request(url) ).read()
 
 ##### Yum Repository Definition for Amazon S3 Repositories #####
 
@@ -258,14 +180,16 @@ class AmazonS3Repo(YumRepository):
 
 	def setupGrab(self):
 		YumRepository.setupGrab(self)
-		self.grabber = AmazonS3Grabber(self.key_id, self.secret_key )
+		logger = logging.getLogger("yum.verbose.main")
+		logger.log(logginglevels.DEBUG_4, "s3: Creating UrllibGrabber")
+		self.grabber = UrllibGrabber(self.key_id, self.secret_key, self.baseurl)
 
 	def _getgrabfunc(self):
 		raise Exception("get grabfunc!")
 
 	def _getgrab(self):
 		if not self.grabber:
-			self.grabber = AmazonS3Grabber(self.key_id, self.secret_key, baseurl=self.baseurl )
+			self.grabber = UrllibGrabber(self.key_id, self.secret_key, baseurl=self.baseurl )
 		return self.grabber
 
 	grabfunc = property(lambda self: self._getgrabfunc())
